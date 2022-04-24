1f32d5f8cc25636810b402954d0ff5ef2200d38e
Paranoid-Dev <du.brisingr.arget@gmail.com>
2022-04-24 20:07:32 +0900

    added example.umm.otcc_macro.c

---
+++ Helper scripts/example.umm.otcc_macro.c
@@ -0,0 +1,95 @@
+#define A f++;}else if(f==
+#define B ){
+#define C free(t);return x;
+#define D printf("%d",x);fflush(stdout);
+#define E printf("%c",x);fflush(stdout);
+#define F scanf("%d", &z);
+#define G if(x==0){
+#define H x=x*y;
+
+int f,t,p,k,x,y,z;
+
+r(i){p=t+i-1;k=*(char*)p;return k;}
+
+c(i,v){p=t+i-1;*(char*)p=v;return 0;}
+
+main(){f=1;t=calloc(10000,40000);for(;;){z=0;y=0;x=0;if(f==1 B
+
+      A 2 B
+      A 3 B
+      x=0;
+      c(1,x);
+      A 4 B
+      x=2;
+      y=-3;
+      H
+      c(2,x);
+      A 5 B
+      A 6 B
+      F
+      x=z;
+      c(3,x);
+      A 7 B
+      A 8 B
+      A 9 B
+      x=2;
+      D
+      A 10 B
+      x=-2;
+      D
+      A 11 B
+      A 12 B
+      x=r(1);
+      D
+      A 13 B
+      x=r(1);
+      x=2;
+      D
+      A 14 B
+      x=r(2);
+      D
+      A 15 B
+      x=r(3);
+      D
+      A 16 B
+      A 17 B
+      x=3;
+      y=3;
+      H
+      y=3;
+      H
+      y=3;
+      H
+      E
+      A 18 B
+      A 19 B
+      x=r(3);
+      G
+      x=r(3);
+      C
+      }
+      A 20 B
+      A 21 B
+      x=2;
+      y=2;
+      H
+      y=2;
+      H
+      y=3;
+      H
+      f=x-1;
+      A 22 B
+      A 23 B
+      x=1;
+      C
+      A 24 B
+      A 25 B
+      x=1;
+      D
+      A 26 B
+      x=0;
+      C
+      A 27 B
+      A 28 B
+
+}}x=0;C}
